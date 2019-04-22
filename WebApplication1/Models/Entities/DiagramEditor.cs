9c039ece9b567faf6845986cf7800571023cc27a
Anton <akhruslov@gmcs.ru>
2019-04-22 17:54:10 +0300

    Login and list

--- WebApplication1/Models/Entities/DiagramEditor.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1.Models.Entities
-{
-    public class DiagramEditor  
-    {
-        public int Id { get; set; }     
-        public string IdentityId { get; set; }   
-        public AppUser Identity { get; set; }  // navigation property
-    }
-}
